9735685749ea566ad0b92441451bbb6fa6fc28b5
shimon2046 <shimon2046@gmail.com>
2022-04-30 20:30:54 +0300

    chat

--- login.js
+++ login.js
@@ -1,12 +1,37 @@
 //alert("connected to webClinet.html")
 
+
 // shift+alt+f -arrange the code
 var inVal = [];
 // name & nick name
 var validname = 1
 var validpass = 1
 var validnick = 1
+var valid = 0
 var myFormData = [];   //declare an array
+
+
+
+
+const users = [
+  { username: 'Brian1', password: 'e1234' },
+  { username: 'Trevor1', password: 'a1234' },
+  { username: 'Jonathan1', password: 'w1234' },
+  { username: 'Joshua1', password: 's1234' },
+  { username: 'Ruth1', password: 'e1234' },
+  { username: 'Anne1', password: 'q1234' },
+  { username: 'Abigail1', password: 'q1234' }];
+
+
+
+
+
+
+
+
+
+
+
 
 
 const form = document.querySelector('.form-container form');
@@ -18,6 +43,8 @@ form.addEventListener('submit', (e) => {
   e.preventDefault(); inputs.forEach(input => {
     // console.log("|" + typeof (input.value) + "|")
     // let errorMsg;
+    console.log("->>>>>1"+ input.value);
+
 
     let messages = []
 
@@ -41,6 +68,15 @@ form.addEventListener('submit', (e) => {
         validname = 0
 
       }
+      console.log("->>>>>2"+ input.value);
+
+
+     
+
+
+
+
+
     }
     //password
     if (input.name == "pswd") {
@@ -53,11 +89,17 @@ form.addEventListener('submit', (e) => {
         validpass = 0
 
       }
+
+    console.log("->>>>>3"+ input.value);
+
       // console.log("password");
     }
   
   });
  //passing to another html file -to chat.html
+
+
+
   redirect();
 
   
@@ -68,7 +110,7 @@ form.addEventListener('submit', (e) => {
 function redirect(event) {
   forr();
   // console.log("validname--->>>>" + typeof (validname));
-  if ((validname == 1) && (validnick == 1) &&  (validpass==1)) {
+  if ((validname == 1) && (validnick == 1) &&  (validpass==1) &&  (valid==1)) {
     window.location.href = "chat.html";
   }
 }
@@ -84,8 +126,21 @@ function forr() {
     var userName = inVal[0];  
     window.localStorage.setItem("0", userName);  
 
-    var nickName = inVal[1];  
-    window.localStorage.setItem("1", nickName);  
+    var password1 = inVal[1];  
+    window.localStorage.setItem("1", password1);  
+
+    console.log("->> "+userName +"nick-<"+password1);
+
+
+     for(let u of users){
+        if((u.username === userName) && (u.password === password1)){
+          valid =1;
+        }
+      }
+
+
+
+
 
 
   }
